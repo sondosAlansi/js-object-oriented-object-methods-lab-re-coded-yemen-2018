65c12fc44f85c1dbb863bebf676313f5a3e36c83
sondos alansi <sondos.alansi@yahoo.com>
2018-10-07 20:21:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ function BoardMember(name,homeState,training){
   this.releasePressStatement  =function(){
     return "You will see great things from Scuber.";
   }
+  this.sayHi   =function(){
+    return "You will see great things from Scuber.";
+  }
   
 }
