3c2d56e63e03564d763e9befd8f720c6a21adc75
sondos alansi <sondos.alansi@yahoo.com>
2018-10-07 20:23:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function BoardMember(name,homeState,training){
     return "You will see great things from Scuber.";
   }
   this.sayHi   =function(){
-    return "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.";
+    return `Hello everybody, my name is ${this.name}!`;
   }
   
 }
