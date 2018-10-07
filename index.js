ecb80e841764ce2265bc468c090d73fbbc1a66a4
sondos alansi <sondos.alansi@yahoo.com>
2018-10-07 20:17:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function BoardMember(name,homeState,training){
   this.name=name;
   this.homeState=homeState;
   this.training=training;
-  this.
+  this.veto =function(){
+    return "";
+  }
   
 }
